fc1a938ff55b731cae1d3a21b6d856c29f6ac148
Salih Bicakci <salih.bicakci@morhipo.com>
2019-02-22 10:06:14 +0300

    Add project files.

---
+++ Fiver.Security.AuthServer.Api/Startup.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiver.Security.AuthServer.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(
+            IServiceCollection services)
+        {
+            //services.AddAuthentication(IdentityServerAuthenticationDefaults.JwtAuthenticationScheme)
+            //        .AddIdentityServerAuthentication(options =>
+            //         {
+            //             options.Authority = "http://localhost:5000"; // Auth Server
+            //             options.RequireHttpsMetadata = false;
+            //             options.ApiName = "fiver_auth_api"; // API Resource Id
+            //         });
+
+            services.AddMvc();
+        }
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            //app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication2
+{
+    using IdentityServer4.AccessTokenValidation;
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                    .AddIdentityServerAuthentication(options =>
+                     {
+                         options.Authority = "http://localhost:5000"; // Auth Server
+                         options.RequireHttpsMetadata = false;
+                         options.ApiName = "fiver_auth_api"; // API Resource Id
+                     });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+            
+        }
+    }
+}
